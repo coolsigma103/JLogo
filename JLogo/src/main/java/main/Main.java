91fca3e7c4c106480f1fa05ca3ae62cd9e68a76b
baodepzaixd <vonguyenbao103@outlook.com.vn>
2025-05-16 17:11:02 +0700

    Create MainWindow

--- JLogo/src/main/java/main/Main.java
+++ JLogo/src/main/java/main/Main.java
@@ -1,8 +1,17 @@
 package main;
 
+import io.mainWindow.Window;
+
 public class Main {
+	
+	public Window window;
+	
+	public Main() {
+		Window window = new Window();
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to JLogo!");
-		
+		new Main();
 	}
 }
